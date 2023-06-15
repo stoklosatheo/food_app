1990f61fd4f8e53b9de021f002217e1940abc195
Théo <stoklosa@adipso.com>
2023-06-15 12:10:24 +0200

    first commit

---
+++ lib/models/recipes_data.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Map<String, String> requestHeaders = {
+  'X-RapidAPI-Key': '425d181a13mshd462af39b9f5cf6p196127jsn1359e085137a',
+  'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+};
+
+Future<RecipeList> fetchRecipeList() async {
+  final response = await http.get(
+      Uri.parse('https://tasty.p.rapidapi.com/recipes/list'),
+      headers: requestHeaders);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return RecipeList.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load recipe');
+  }
+}
+
+class RecipeList {
+  RecipeList({required this.recipes});
+
+  List<Recipe> recipes;
+
+  factory RecipeList.fromJson(Map<String, dynamic> json) {
+    // print(List<Recipe>.from(json['results'].map((x) => x['name'])));
+    return RecipeList(
+      recipes:
+          List<Recipe>.from(json['results'].map((x) => Recipe.fromJson(x))),
+    );
+  }
+}
+
+class Recipe {
+  const Recipe({required this.name, required this.thumbnail, this.tags});
+
+  final String name;
+  final String thumbnail;
+  final List<String>? tags;
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(name: json['name'], thumbnail: json['thumbnail_url']);
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/models/recipes_data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<RecipeList> futureRecipe;
+
+  @override
+  void initState() {
+    super.initState();
+    futureRecipe = fetchRecipeList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(children: [
+                  FutureBuilder<RecipeList>(
+                    future: futureRecipe,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return SizedBox(
+                            height: 600,
+                            child: ListView.separated(
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                      elevation: 3,
+                                      child: Container(
+                                          height: 200,
+                                          decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              image: DecorationImage(
+                                                  opacity: 0.8,
+                                                  fit: BoxFit.cover,
+                                                  image: NetworkImage(snapshot
+                                                      .data!
+                                                      .recipes[index]
+                                                      .thumbnail))),
+                                          child: Padding(
+                                              padding: const EdgeInsets.all(15),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.end,
+                                                children: [
+                                                  Text(
+                                                      style: const TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          fontSize: 20),
+                                                      snapshot.data!
+                                                          .recipes[index].name
+                                                          .toUpperCase()),
+                                                ],
+                                              ))));
+                                },
+                                separatorBuilder: (context, index) {
+                                  return const SizedBox(height: 10);
+                                },
+                                itemCount: 3));
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+
+                      // By default, show a loading spinner.
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                ]))));
+  }
+}
