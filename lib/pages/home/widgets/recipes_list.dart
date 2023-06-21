198a25b24ea0836af66aa998e186adec02d0df38
Théo <stoklosa@adipso.com>
2023-06-21 11:37:31 +0200

    fix conflits

--- lib/pages/home/widgets/recipes_list.dart
+++ lib/pages/home/widgets/recipes_list.dart
@@ -11,6 +11,14 @@ class RecipesList extends StatefulWidget {
 class _RecipesListState extends State<RecipesList> {
   late Future<RecipeList> futureRecipe;
 
+  List<Color> tagColors = [
+    const Color.fromRGBO(244, 68, 55, 0.8),
+    const Color.fromRGBO(101, 181, 244, 0.8),
+    const Color.fromRGBO(78, 209, 226, 0.8),
+    const Color.fromRGBO(245, 143, 109, 0.8),
+    const Color.fromRGBO(253, 103, 129, 0.8),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -54,13 +62,68 @@ class _RecipesListState extends State<RecipesList> {
                                               fontSize: 20),
                                           snapshot.data!.recipes[index].name
                                               .toUpperCase()),
+                                      Wrap(
+                                        spacing: 10,
+                                        runSpacing: 5,
+                                        children: [
+                                          if (snapshot.data!.recipes[index]
+                                                  .tags!.length >
+                                              3)
+                                            for (var i = 0; i < 3; i++)
+                                              Container(
+                                                  color: tagColors[i],
+                                                  child: Padding(
+                                                      padding: const EdgeInsets
+                                                              .symmetric(
+                                                          vertical: 5,
+                                                          horizontal: 5),
+                                                      child: Text(
+                                                          snapshot
+                                                              .data!
+                                                              .recipes[index]
+                                                              .tags![i]
+                                                              .displayName
+                                                              .toUpperCase(),
+                                                          style:
+                                                              const TextStyle(
+                                                                  color: Colors
+                                                                      .white))))
+                                          else
+                                            for (var i = 0;
+                                                i <
+                                                    snapshot
+                                                        .data!
+                                                        .recipes[index]
+                                                        .tags!
+                                                        .length;
+                                                i++)
+                                              Container(
+                                                  color: tagColors[i],
+                                                  child: Padding(
+                                                      padding: const EdgeInsets
+                                                              .symmetric(
+                                                          vertical: 5,
+                                                          horizontal: 5),
+                                                      child: Text(
+                                                          snapshot
+                                                              .data!
+                                                              .recipes[index]
+                                                              .tags![i]
+                                                              .displayName
+                                                              .toUpperCase(),
+                                                          style:
+                                                              const TextStyle(
+                                                                  color: Colors
+                                                                      .white))))
+                                        ],
+                                      )
                                     ],
                                   ))));
                     },
                     separatorBuilder: (context, index) {
                       return const SizedBox(height: 10);
                     },
-                    itemCount: 3));
+                    itemCount: snapshot.data!.recipes.length));
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
